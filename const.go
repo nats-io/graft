ecf0d9f8825bc5363adb635833b9cccaa7f14dc1
Derek Collison <derek@apcera.com>
2013-07-19 10:56:56 -0500

    Moved State to own file

--- const.go
+++ const.go
@@ -3,7 +3,6 @@
 package graft
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -14,7 +13,7 @@ const (
 	MIN_ELECTION_TIMEOUT = 500 * time.Millisecond
 	MAX_ELECTION_TIMEOUT = 2 * MIN_ELECTION_TIMEOUT
 
-	// Heartbeat tick for LEADERS. Should be << MIN_ELECTION
+	// Heartbeat tick for LEADERS. Should be << MIN_ELECTION_TIMEOUT
 	HEARTBEAT_INTERVAL = 100 * time.Millisecond
 
 	NO_LEADER = ""
@@ -23,29 +22,3 @@ const (
 	// Use buffer channels.
 	CHAN_SIZE = 8
 )
-
-type State int8
-
-// Allowable states for a Graft node.
-const (
-	FOLLOWER State = iota
-	LEADER
-	CANDIDATE
-	CLOSED
-)
-
-// Convenience for printing, etc.
-func (s State) String() string {
-	switch s {
-	case FOLLOWER:
-		return "Follower"
-	case LEADER:
-		return "Leader"
-	case CANDIDATE:
-		return "Candidate"
-	case CLOSED:
-		return "Closed"
-	default:
-		return fmt.Sprintf("Unknown[%d]", s)
-	}
-}
